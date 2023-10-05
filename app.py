009fda11ec1b6ccfe87f97083da8a7c16e241cf3
Chandima Ranaweera <chan4lk@gmail.com>
2023-10-05 23:16:18 +0530

    changed sidebar

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ bistec_bot.add("https://bistecglobal.com/case-studies/")
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    "[View the source code](https://github.com/chan4lk/hello-embedchain/blob/main/app.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/chan4lk/hello-embedchain?quickstart=1)"
 
 st.title("💬 Chatbot") 
 
