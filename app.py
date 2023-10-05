eb5823f61043c6cae60e7a00553eff14af353978
Chandima Ranaweera <chan4lk@gmail.com>
2023-10-05 18:28:32 +0530

    embedchain chat added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from embedchain import App
+import streamlit as st
 
 bistec_bot = App()
 
@@ -7,5 +8,30 @@ bistec_bot.add("https://bistecglobal.com/")
 bistec_bot.add("https://bistecglobal.com/life/")
 bistec_bot.add("https://bistecglobal.com/case-studies/")
 
-response = bistec_bot.query("What is hearts academy ?")
-print(response)
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("💬 Chatbot") 
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    os.environ["OPENAI_API_KEY"]  = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    response = bistec_bot.query(prompt)
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(response)
